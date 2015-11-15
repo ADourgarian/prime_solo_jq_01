e2c7c5f425e9871f1d4e23803f70871fc64b3812
Andrew Dourgarian <Dragon@Greggs-MacBook-Air.local>
2015-11-14 20:43:58 -0600

    Finished all requirements, not perfect

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ $(document).ready(function(){
 	//  update displayed employees list after each submit
 	function listEmployee(employee){
 		var $div = $('<div>');
+		var $ulX = $('<ul>');
 		for (var i=0;i<employee.length;i++){
 			//Creates new tags for appends
 			
-			var $ulX = $('<ul>');
-			var $liName = $('<li>');
-			var $liValue = $('<li>');
+			var $li = $('<li>');
 
 			//create lists
-			$liName.append(employee[i].name);
-			$liValue.append(employee[i].value);
+			$li.append(employee[i].name).append(': ').append(employee[i].value);
 
-			$ulX.append($liName).append($liValue);
-			$div.append($ulX);
-
+			$ulX.append($li)
+			
 		}
 		// append all form content
+		console.log(employee[4].value);
+		switch (employee[4].value){
+		case '1': $ulX.addClass('red')
+		break;
+		case '2': $ulX.addClass('orange')
+		break;
+		case '3': $ulX.addClass('yellow')
+		break;
+		case '4': $ulX.addClass('blue')
+		break;
+		case '5': $ulX.addClass('green')
+		break;
+		};
+		$div.append($ulX);
+		$div.addClass('float');
 		$('.content').append($div);
 
 		//Add Button to Delete employee
@@ -32,11 +44,8 @@ $(document).ready(function(){
 			$div.append($button);
 		}
 		removeEmployee();
-	};
 
-	$('.createEmp').on('click',function(){
-		$('form').removeClass('display','hide')
-	});
+	};
 
 	//Store form results on submit
 	$('form').on('submit',function(){
@@ -45,10 +54,10 @@ $(document).ready(function(){
 		employees.push(employee);
 
 		listEmployee(employee);
-
-
+		$('form')[0].reset();
 
 	});
+
 })
 
 
